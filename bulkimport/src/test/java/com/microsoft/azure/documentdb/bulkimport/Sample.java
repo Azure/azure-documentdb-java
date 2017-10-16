fe70d0736d59194aa101c69ce072bf1996833fbb
Mohammad Derakhshani <moderakh@users.noreply.github.com>
2017-10-15 23:09:54 -0700

    added readme.txt

--- bulkimport/src/test/java/com/microsoft/azure/documentdb/bulkimport/Sample.java
+++ bulkimport/src/test/java/com/microsoft/azure/documentdb/bulkimport/Sample.java
@@ -22,10 +22,7 @@
  */
 package com.microsoft.azure.documentdb.bulkimport;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 import com.microsoft.azure.documentdb.ConnectionPolicy;
@@ -50,6 +47,14 @@ public class Sample {
 
             try(BulkImporter importer = new BulkImporter(client, collection)) {
 
+                //NOTE: for getting higher throughput please
+                // 1)  Set JVM heap size to a large enough number to avoid any memory issue in handling large number of documents 
+                // 2)  there is a pre-processing and warm up time and due that,
+                //     you will get higher throughput for bulks with larger number of documents. 
+                //     So if you want to import 10,000,000 documents, 
+                //     running bulk import 10 times on 10 bulk of documents each of size 1,000,000 is more preferable
+                //     than running bulk import 100 times on 100 bulk of documents each of size 100,000 documents. 
+   
                 for(int i = 0; i< 10; i++) {
                     Collection<String> docs = DataMigrationDocumentSource.loadDocuments(1000000, collection.getPartitionKey());
                     BulkImportResponse bulkImportResponse = importer.bulkImport(docs, false);
