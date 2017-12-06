ffb30633d1bf01f6b0e9945e219421d3c4b814d5
Chebrolu Bala Sai Harika <34155580+chebroluharika@users.noreply.github.com>
2017-12-06 14:52:38 +0530

    Added Upsert document functionality.

---
+++ src/GetStarted/Teacher.java
@@ -0,0 +1,30 @@
+package GetStarted;
+
+public class Teacher{
+
+    public Teacher() {
+    }
+
+    public Teacher(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    private String LastName;
+    private String firstName;
+}
