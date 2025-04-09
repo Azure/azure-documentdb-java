ed743642563492fd74d8ae5cfee69feb00b1788f
Fabian Meiswinkel <fabianm@microsoft.com>
2025-04-09 01:03:23 +0000

    Repro of exact pages

--- documentdb-examples/src/test/java/com/microsoft/azure/documentdb/examples/DocumentQuerySamples.java
+++ documentdb-examples/src/test/java/com/microsoft/azure/documentdb/examples/DocumentQuerySamples.java
@@ -26,11 +26,24 @@ package com.microsoft.azure.documentdb.examples;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import java.lang.reflect.Field;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Pattern;
 
+import com.microsoft.azure.documentdb.ConnectionMode;
+import com.microsoft.azure.documentdb.ConnectionPolicy;
+import com.microsoft.azure.documentdb.FeedResponse;
+import com.microsoft.azure.documentdb.QueryIterable;
+import com.microsoft.azure.documentdb.internal.GatewayProxy;
 import org.apache.commons.lang3.RandomUtils;
 import org.junit.After;
 import org.junit.Before;
@@ -52,8 +65,8 @@ import com.microsoft.azure.documentdb.RequestOptions;
 
 public class DocumentQuerySamples
 {
-    private final String databaseId = "exampleDB";
-    private final String collectionId = UUID.randomUUID().toString();
+    private final String databaseId = "CRI";
+    private final String collectionId = "AdobeQuery";
     private final String partitionKeyFieldName = "city";
     private final String partitionKeyPath = "/" + partitionKeyFieldName;
     private final String collectionLink = String.format("/dbs/%s/colls/%s", databaseId, collectionId);
@@ -63,146 +76,188 @@ public class DocumentQuerySamples
     @Before
     public void setUp() throws DocumentClientException {
         // create client
-        client = new DocumentClient(AccountCredentials.HOST, AccountCredentials.MASTER_KEY, null, null);
+        ConnectionPolicy policy = new ConnectionPolicy();
+        policy.setConnectionMode(ConnectionMode.Gateway);
+        client = new DocumentClient(AccountCredentials.HOST, AccountCredentials.MASTER_KEY, policy, null);
 
         // removes the database "exampleDB" (if exists)
-        deleteDatabase();
+        //deleteDatabase();
         // create database exampleDB;
-        createDatabase();
+        //createDatabase();
 
         // create collection
-        createMultiPartitionCollection();
+        //createMultiPartitionCollection();
 
         // populate documents
-        populateDocuments();
+        //populateDocuments();
     }
 
     @After
     public void shutDown() {
-        deleteDatabase();
         if (client != null) {
             client.close();
         }
     }
 
-    private void populateDocuments() throws DocumentClientException {
-
-        String[] cities = new String [] { "Amherst", "Seattle", "Redmond", "北京市", "बंगलौर - विकिपीडिया", "شیراز‎‎" };
-        String[] poetNames = new String[] { "Emily Elizabeth Dickinson", "Hafez", "Lao Tzu" };
-
-        for(int i = 0; i < 100; i++) {
-            Document documentDefinition = new Document();
-            documentDefinition.setId("test-document" + i);
-            documentDefinition.set("city", cities[i % cities.length]);
-            documentDefinition.set("cnt", i);
-
-            documentDefinition.set("poetName", poetNames[ i % poetNames.length ]);
-            documentDefinition.set("popularity", RandomUtils.nextDouble(0, 1));
-
-            client.createDocument(collectionLink, documentDefinition, null, true);
-
+    private void applyCorrelatedActivityIdViaReflection(String correlatedActivityId) {
+        Field gatewayProxyField = Arrays.asList(DocumentClient.class.getDeclaredFields())
+                                        .stream()
+                                        .filter(field -> field.getName().equals("gatewayProxy"))
+                                        .findFirst().orElseThrow(() -> new RuntimeException("Field not found"));
+        gatewayProxyField.setAccessible(true);
+        Object gatewayProxy = null;
+        try {
+            gatewayProxy = gatewayProxyField.get(client);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
         }
+
+        Field defaultHeadersField = Arrays.asList(GatewayProxy.class.getDeclaredFields())
+                                          .stream()
+                                          .filter(field -> field.getName().equals("defaultHeaders"))
+                                          .findFirst().orElseThrow(() -> new RuntimeException("Field not found"));
+        defaultHeadersField.setAccessible(true);
+
+        Map<String, String> defaultHeaders = null;
+        try {
+            defaultHeaders = (Map<String, String>) defaultHeadersField.get(gatewayProxy);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("CORRELATED_ACTIVITY_ID: " + correlatedActivityId);
+        defaultHeaders.put("x-ms-cosmos-correlated-activityid", correlatedActivityId);
+        defaultHeaders.put("User-Agent", "SampleQuery" + correlatedActivityId);
+    }
+
+    private String parseCosmosContinuation(String firstContinuationFragment) {
+        if (firstContinuationFragment == "") {
+            return null;
+        }
+
+        byte[] cosmosContinuationBlob = Base64.getDecoder().decode(firstContinuationFragment);
+        return new String(cosmosContinuationBlob, StandardCharsets.UTF_8);
+    }
+
+    private Page queryCosmosWithPagination(
+        String whereClause,
+        String ct, // Format: <CosmosContinuation>|PKRangeIdOfLastDocOnPreviousPage|_rid-OfLastDocOnPreviousPage
+        int pageSize) {
+
+        List<Document> docs = new ArrayList<>();
+        final AtomicInteger remainingPageSize = new AtomicInteger(pageSize);
+        String skipDocumentsIncludingResourceId = null;
+        String skipDocumentsPkRangeId = null;
+        FeedOptions options = new FeedOptions();
+        options.setEnableCrossPartitionQuery(true);
+        options.setResponseContinuationTokenLimitInKb(1);
+        options.setPageSize(remainingPageSize.get());
+        String nextPageContinuation = null;
+        String currentPageContinuation = null;
+        if (ct != null) {
+            String[] continuationFragments = ct.split(Pattern.quote("|"));
+            String cosmosContinuationToken = parseCosmosContinuation(continuationFragments[0]);
+            nextPageContinuation = cosmosContinuationToken;
+            options.setRequestContinuation(cosmosContinuationToken);
+            skipDocumentsIncludingResourceId = continuationFragments.length == 3 ? continuationFragments[1] : null;
+            skipDocumentsPkRangeId = continuationFragments.length == 3 ? continuationFragments[2] : null;
+        }
+        String sqlQuery = whereClause != null
+            ? "SELECT * from c where " + whereClause
+            : "SELECT * from c";
+
+        FeedResponse<Document> queryResults = client.queryDocuments(
+            collectionLink, sqlQuery, options);
+        Iterator<Document> docIterator = queryResults.getQueryIterator();
+        String previousActivityId = null;
+        String lastResourceId = "";
+        String lastResourceIdPkRangeId = "";
+        while (remainingPageSize.get() > 0) {
+            if (docIterator.hasNext()) {
+                String activityId = queryResults.getActivityId();
+                // If activityId changed it means we are on the first document of a new page
+                // If so, updating the continuations and logging ActivityId and headers
+                if (activityId != previousActivityId) {
+                    previousActivityId = activityId;
+                    System.out.println("New FeedResponse ActivityId " + activityId);
+                    currentPageContinuation = nextPageContinuation;
+                    nextPageContinuation = queryResults.getResponseContinuation();
+                    System.out.println("New FeedResponse Continuation " + currentPageContinuation);
+                    System.out.println("New FeedResponse CurrentContinuation " + nextPageContinuation);
+                    System.out.println("New FeedResponse Headers " + queryResults.getResponseHeaders());
+                    System.out.println("-------------");
+                }
+
+                Document doc = docIterator.next();
+                String currentPkRangeId = queryResults.getResponseHeaders().get("x-ms-documentdb-partitionkeyrangeid");
+
+                // If we are still on the same PKRangeId from which documents were served
+                // on the previous page (part of Continuation) then  skip all
+                // docs with _rid <= the rid of the last document returned on the previous page
+                if (!currentPkRangeId.equals(skipDocumentsPkRangeId)
+                    || skipDocumentsIncludingResourceId == null
+                    || skipDocumentsIncludingResourceId.compareTo(doc.getResourceId()) < 0) {
+
+                    remainingPageSize.decrementAndGet();
+                    docs.add(doc);
+                    lastResourceId = doc.getResourceId();
+                    lastResourceIdPkRangeId = queryResults.getResponseHeaders().get("x-ms-documentdb-partitionkeyrangeid");
+                } else {
+                    System.out.println(
+                        "Skipping doc " + doc.getId() + "("
+                            + currentPkRangeId + "|" + doc.getResourceId()
+                            + ") because it was returned on previous page already");
+                }
+            } else {
+                // Query has been fully drained - just return the docs collected so far
+                return new Page(docs, null);
+            }
+        }
+
+        String base64EncodedCosmosContinuation = currentPageContinuation != null
+            ? Base64.getEncoder().encodeToString(currentPageContinuation.getBytes(StandardCharsets.UTF_8))
+            : "";
+        String returnContinuation = base64EncodedCosmosContinuation
+            + "|"
+            + lastResourceId
+            + "|"
+            + lastResourceIdPkRangeId;
+
+        return new Page(docs, returnContinuation);
     }
 
     @Test
     public void simpleDocumentQuery() throws DocumentClientException {
 
-        // as this is a multi collection enable cross partition query
-        FeedOptions options = new FeedOptions();
-        options.setEnableCrossPartitionQuery(true);
+        applyCorrelatedActivityIdViaReflection(UUID.randomUUID().toString());
+        String ct = null;
 
-        Iterator<Document> it = client.queryDocuments(collectionLink, "SELECT * from r", options).getQueryIterator();
+        int totalCount = 0;
+        do {
+            Page page = queryCosmosWithPagination(null, ct, 2);
+            System.out.println("CONTINUATION: " + page.getContinuation());
+            System.out.println(page.getDocs().size() + " docs");
+            totalCount += page.getDocs().size();
+            for (Document doc : page.getDocs()) {
+                System.out.println("  - " + doc.getId());
+            }
 
-        int i = 0;
-        while(it.hasNext()) {
-            Document d = it.next();
+            ct = page.getContinuation();
+        } while (ct != null );
 
-            System.out.println("id is " + d.getId());
-            System.out.println("citi is " + d.getString("city"));
-            System.out.println("cnt is " + d.getInt("cnt"));
-            System.out.println("popularity is " + d.getDouble("popularity"));
+        System.out.println("TOTAL DOC COUNT: " + totalCount);
+    }
 
-            i++;
+    private static class Page {
+        private final List<Document> docs;
+        private final String continuation;
+
+        public Page(List<Document> docs, String continuation) {
+            this.docs = docs;
+            this.continuation = continuation;
         }
 
-        assertThat(i, equalTo(100));
-    }
-
-    @Test
-    public void orderByQuery() throws DocumentClientException {
-
-        // as this is a multi collection enable cross partition query
-        FeedOptions options = new FeedOptions();
-        options.setEnableCrossPartitionQuery(true);
-
-        Iterator<Document> it = client.queryDocuments(collectionLink, "SELECT * from r ORDER BY r.cnt", options).getQueryIterator();
-
-        int i = 0;
-        while(it.hasNext()) {
-            Document d = it.next();
-
-            System.out.println("id is " + d.getId());
-            System.out.println("citi is " + d.getString("city"));
-            System.out.println("cnt is " + d.getInt("cnt"));
-            System.out.println("popularity is " + d.getDouble("popularity"));
-
-            assertThat(d.getInt("cnt"), equalTo(i));
-
-            i++;
-        }
-        assertThat(i, equalTo(100));
-    }
-
-    private void createDatabase() throws DocumentClientException {
-        Database databaseDefinition = new Database();
-        databaseDefinition.setId(databaseId);
-        client.createDatabase(databaseDefinition, null);
-    }
-
-    private void createMultiPartitionCollection() throws DocumentClientException {
-
-        String databaseLink = String.format("/dbs/%s", databaseId);
-
-        DocumentCollection collectionDefinition = new DocumentCollection();
-        collectionDefinition.setId(collectionId);
-
-        // set /city as the partition key path
-        PartitionKeyDefinition partitionKeyDefinition = new PartitionKeyDefinition();
-        Collection<String> paths = new ArrayList<String>();
-        paths.add(partitionKeyPath);
-        partitionKeyDefinition.setPaths(paths);
-        collectionDefinition.setPartitionKey(partitionKeyDefinition);
-
-        // set the throughput to be 10,200
-        RequestOptions options = new RequestOptions();
-        options.setOfferThroughput(10200);
-
-        // set indexing policy to be range range for string and number
-        IndexingPolicy indexingPolicy = new IndexingPolicy();
-        Collection<IncludedPath> includedPaths = new ArrayList<IncludedPath>();
-        IncludedPath includedPath = new IncludedPath();
-        includedPath.setPath("/*");
-        Collection<Index> indexes = new ArrayList<Index>();
-        Index stringIndex = Index.Range(DataType.String);
-        stringIndex.set("precision", -1);
-        indexes.add(stringIndex);
-
-        Index numberIndex = Index.Range(DataType.Number);
-        numberIndex.set("precision", -1);
-        indexes.add(numberIndex);
-        includedPath.setIndexes(indexes);
-        includedPaths.add(includedPath);
-        indexingPolicy.setIncludedPaths(includedPaths);
-        collectionDefinition.setIndexingPolicy(indexingPolicy);
-
-        // create a collection
-        client.createCollection(databaseLink, collectionDefinition, options);
-    }
-
-    private void deleteDatabase() {
-        try {
-            client.deleteDatabase(String.format("/dbs/%s", databaseId), null);
-        } catch (Exception e) {
-        }
+        public List<Document> getDocs() { return this.docs; }
+        public String getContinuation() { return this.continuation; }
     }
 }
